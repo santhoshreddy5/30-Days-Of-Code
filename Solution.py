d26d06ac66aa8b63da96b2adbae7d6818c97e2e6
Santhosh Kumar Reddy G <115708150+santhoshreddy5@users.noreply.github.com>
2023-03-19 18:19:37 +0530

    Delete Solution.py

--- Solution.py
+++
@@ -1,9 +0,0 @@
-meal_cost = float(input())
-tip_percent = int(input())
-tax_percent = int(input())
-
-tip = meal_cost * tip_percent / 100
-tax = meal_cost * tax_percent / 100
-total = meal_cost + tip + tax
-
-print("The total meal cost is " + str(round(total)) + " dollars.")
